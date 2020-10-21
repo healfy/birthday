4561780b0716b800d48bd2a4eaee17fe2e79401a
Pavel Sturov <paul.sturov@gmail.com>
2020-10-21 21:06:17 +0300

    Think different.

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@ python manage.py makemigrations
 python manage.py migrate
 echo "yes" | python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8000
+# вот эти команды
+# первые две делают миграции в базе, они впринципе не нужны
+# третья можно без
